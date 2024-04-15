0263b95d1d64b08406d885b499019a14faaff0fb
Vasiliy <vemelin@gmail.com>
2024-04-15 08:34:45 -0500

    fixing image placeholder when there is no preview

--- src/components/Main/List/Post/Post.jsx
+++ src/components/Main/List/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React} from 'react';
 import noPhoto from './assets/placeholder.jpeg';
 import style from './Post.module.css';
 import propTypes from 'prop-types';
@@ -15,47 +15,54 @@ import {useReddit} from '../../../../hooks/RedditProvider';
 /* eslint-disable react/no-unknown-property */
 
 const Post = () => {
-  // const {title, author, ups, date} = postData;
   const {posts, loading} = useReddit();
+  // const [imageLoaded, setImageLoaded] = useState(false);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  // const img = document.querySelectorAll('img');
-  // img.forEach(el => {
-  //   console.log(el.complete);
-  //   console.log(el);
-  //   // if (!el.complete) {
-  //   //   // The image is broken
-  //   // }
-  // });
+  const img = document.querySelectorAll('img');
+  img.forEach(el => {
+    console.log(el.onload);
+    if (el.naturalWidth === 0 && el.naturalHeight === 0) {
+      // The image does not have a preview
+      console.log('test');
+    } else {
+      console.log(el);
+      // The image has a preview
+    }
+  });
+
+  const handleImageLoad = () => {
+    console.log(img);
+    // Set imageLoaded to true when the image has finished loading
+    // setImageLoaded(true);
+  };
+
 
   return (
-    posts.map(data => {
-      return (
-        <li className={style.post} key={data.id}>
-          { data.thumbnail &&
-            data.thumbnail !== 'self' &&
-            data.thumbnail !== 'default' ? (
-              <Image src={data.thumbnail} title={data.title} />
+    posts.map(data => (
+      <li className={style.post} key={data.id}>
+        { posts ? (
+              <Image src={data.thumbnail} title={data.title}
+                onLoad={handleImageLoad} />
           ) : (
             <Image src={noPhoto} title={data.title} />
           )}
-          <div className={style.content}>
-            <Title title={data.title} />
-            <AuthorName authorName={data.author} />
-            <Button control={'delete'}/>
-          </div>
-          <div className={style.rating}>
-            <Button control={'up'} />
-            <Rating label={data.ups} />
-            <Button control={'down'}/>
-          </div>
-          <PostDate dateTime={data.created} />
-        </li>
-      );
-    })
+        <div className={style.content}>
+          <Title title={data.title} />
+          <AuthorName authorName={data.author} />
+          <Button control={'delete'}/>
+        </div>
+        <div className={style.rating}>
+          <Button control={'up'} />
+          <Rating label={data.ups} />
+          <Button control={'down'}/>
+        </div>
+        <PostDate dateTime={data.created} />
+      </li>
+    ))
   );
 };
 
